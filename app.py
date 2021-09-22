595d7a148864694c1edd904c795b105606db85e0
Oneday2010 <jozxz1691@gmail.com>
2021-09-22 05:09:21 +0000

    add subscribe page

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +23,11 @@ mongo = PyMongo(app)
 def get_foods():
     foods = mongo.db.foods.find()
     return render_template("foods.html", foods=foods)
+
+
+@app.route("/subscribe", methods=["GET", "POST"])
+def subscribe():
+    return render_template("subscribe.html")   
 
 
 if __name__ == "__main__":
