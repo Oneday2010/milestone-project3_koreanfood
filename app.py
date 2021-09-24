685a9e7c2a5b2ca28b4500f7e4cc384e46eafa53
Oneday2010 <jozxz1691@gmail.com>
2021-09-24 05:24:08 +0000

    add registeration function

--- app.py
+++ app.py
@@ -25,9 +25,27 @@ def get_foods():
     return render_template("foods.html", foods=foods)
 
 
-@app.route("/subscribe", methods=["GET", "POST"])
-def subscribe():
-    return render_template("subscribe.html")   
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
