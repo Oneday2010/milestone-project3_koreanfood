2407bd4b5daf0b831bcff76abcb21b8096d4fae5
Oneday2010 <jozxz1691@gmail.com>
2021-10-01 18:29:23 +0000

    add food images

--- env.py
+++ env.py
@@ -5,3 +5,4 @@ os.environ.setdefault("PORT", "5000")
 os.environ.setdefault("SECRET_KEY", "vUM0v8rDdG")
 os.environ.setdefault("MONGO_URI", "mongodb+srv://augustProject:BDDgzs93RZ1eQQV8@myfirstcluster.r58bd.mongodb.net/korean_food?retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DBNAME", "korean_food")
+
