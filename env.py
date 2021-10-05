94d322f2488365a9f4fc475886de666b37fda2de
Oneday2010 <jozxz1691@gmail.com>
2021-10-05 18:41:00 +0000

    delate file

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "vUM0v8rDdG")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://augustProject:octoberProject2021@myfirstcluster.r58bd.mongodb.net/korean_food?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "korean_food")
-
